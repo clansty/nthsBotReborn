061a7b7b1993fa52aa6f653adf9ed5404226956b
Clansty <i@gao4.pw>
2021-07-13 15:53:55 +0800

    配置文件

---
+++ index.ts
@@ -0,0 +1,3 @@
+import {config} from './managers/configManager'
+
+
---
+++ managers/configManager.ts
@@ -0,0 +1,43 @@
+import fs from 'fs'
+import YAML from 'yaml'
+
+type Config={
+    account:{
+        /**
+         * QQ号
+         */
+        qq:number
+        /**
+         * 密码
+         */
+        passwd: string
+    }
+    groups:{
+        /**
+         * 主群群号
+         */
+        main:number
+        /**
+         * 管理群群号
+         */
+        admin:number
+    }
+    /**
+     * MongoDB 连接字符串
+     */
+    db:string
+    /**
+     * 库名
+     */
+    dbName: string
+}
+
+export let config:Config
+if (fs.existsSync('config.yaml')) {
+    config = YAML.parse(fs.readFileSync('config.yaml', 'utf8'))
+} else {
+    console.error('配置文件不存在')
+    process.exit(2)
+}
+
+
