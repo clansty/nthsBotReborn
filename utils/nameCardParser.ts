00b8101c2565fcd0b021e54e07bf634b6d6c8614
Clansty <i@gao4.pw>
2021-07-18 13:30:28 +0800

    信息解析

--- utils/nameCardParser.ts
+++ utils/nameCardParser.ts
@@ -1,8 +1,16 @@
 import {enrollments} from '../maps/enrollments'
 
-export const parseBranch = (name) => /金阊/.test('name')
-export const parseEnrollment = (name) => {
+export const parseBranch = (name: string) => /金阊/.test(name)
+export const parseEnrollment = (name: string) => {
     if (/高[一1]/.test(name)) return enrollments[1]
     if (/高[二2]/.test(name)) return enrollments[2]
     if (/高[三3]/.test(name)) return enrollments[3]
+}
+export const parseJunior = (name: string) => /届初中/.test(name)
+export const parseNick = (name: string) => {
+    name = name.replace(/[丨｜]/g, ' | ')
+    const index = name.lastIndexOf('|')
+    if (index)
+        return name.substr(index + 1).trim()
+    return name.trim()
 }
